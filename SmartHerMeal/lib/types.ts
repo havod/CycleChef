cc08ebb68aea4588ef6033b8ac3aab1b497f5434
havod <hardy.vodounou@polymtl.ca>
2025-11-22 13:40:34 -0500

    update on llib

---
+++ SmartHerMeal/lib/types.ts
@@ -0,0 +1,48 @@
+
+export type UserProfile = {
+  name?: string;
+  age?: number;
+  gender?: 'F' | 'M' | 'Non defined';
+  country?: string;
+  dietaryPreferences?: string[];
+  otherDietaryPreference?: string;
+  menstrualCycle?: 'regular' | 'irregular';
+  healthConditions?: string[];
+  weight?: number;
+  weightUnit?: 'kg' | 'lbs';
+  height?: number | string;
+  heightUnit?: 'cm' | 'ft';
+  nutritionalGoals?: string[];
+  activityLevel?: 'very' | 'not' | 'medium';
+  budget?: number;
+  budgetFrequency?: 'weekly' | 'bi-weekly' | 'monthly';
+  preferences?: string;
+};
+
+export type ImagePlaceholder = {
+  id: string;
+  description: string;
+  imageUrl: string;
+  imageHint: string;
+};
+
+export type Recipe = {
+  id:string;
+  title: string;
+  description: string;
+  ingredients: string[];
+  instructions: string[];
+  cyclePhase: ('Menstrual' | 'Follicular' | 'Ovulatory' | 'Luteal')[];
+  image?: ImagePlaceholder;
+  notes?: string;
+};
+
+export type BlogPost = {
+  slug: string;
+  title: string;
+  description: string;
+  author: string;
+  date: string;
+  image?: ImagePlaceholder;
+  content: string;
+};
